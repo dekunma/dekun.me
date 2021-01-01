a2917a8a7cf66b27fe04cba212e9aff077988e33
dekunma <William@dekun.me>
2021-01-01 17:32:57 +0800

    less style bug fix

--- craco.config.js
+++ craco.config.js
@@ -7,7 +7,7 @@ module.exports = {
       options: {
         lessLoaderOptions: {
           lessOptions: {
-            modifyVars: { '@primary-color': '#1DA57A' },
+            modifyVars: { '@primary-color': '#f05f40' },
             javascriptEnabled: true,
           },
         },
