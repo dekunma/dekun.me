caa89222275971f39da9ba4f1bfa1cc2ee21687a
Dekun Ma <william@dekun.me>
2022-01-31 08:59:36 -0500

    Update console log

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ window.addEventListener("load", () => {
   if (!container) return;
 
   container.forEach((element) => {
-
     const classNames = ["orange", "blue", "red", "pink", "yellow"];
 
     const rawString = element.textContent;
@@ -12,26 +11,26 @@ window.addEventListener("load", () => {
 
     for (let i = 0; i < rawString.length; i++) {
       if (rawString.substring(i, i + 3) === "NYU") {
-				i += 2;
-				const nyu = document.createElement("a");
-				nyu.textContent = "NYU";
-				nyu.href = 'https://www.nyu.edu/';
-				nyu.target = '_blank';
-				nyu.classList.add('violet');
-				element.appendChild(nyu);
-				continue
-			}
+        i += 2;
+        const nyu = document.createElement("a");
+        nyu.textContent = "NYU";
+        nyu.href = "https://www.nyu.edu/";
+        nyu.target = "_blank";
+        nyu.classList.add("violet");
+        element.appendChild(nyu);
+        continue;
+      }
 
-			if (rawString.substring(i, i + 12) === "ThoughtWorks") {
-				i += 11;
-				const nyu = document.createElement("a");
-				nyu.textContent = "ThoughtWorks";
-				nyu.href = 'https://www.thoughtworks.com/';
-				nyu.target = '_blank';
-				nyu.classList.add('tw');
-				element.appendChild(nyu);
-				continue
-			}
+      if (rawString.substring(i, i + 12) === "ThoughtWorks") {
+        i += 11;
+        const nyu = document.createElement("a");
+        nyu.textContent = "ThoughtWorks";
+        nyu.href = "https://www.thoughtworks.com/";
+        nyu.target = "_blank";
+        nyu.classList.add("tw");
+        element.appendChild(nyu);
+        continue;
+      }
 
       const character = rawString[i];
 
