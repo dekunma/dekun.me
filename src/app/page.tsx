cba17461c0acb0061be635623112ec2411b454aa
Dekun Ma <william@dekun.me>
2025-01-19 11:44:11 -0800

    fix: notion api caching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { NotionRenderer } from "react-notion";
 import CommentSection from "@/components/CommentSection";
 
 export default async function Home() {
-  const res = await fetch('https://notion-api.splitbee.io/v1/page/180aa21a2f79807b8f08cbcb8370c282');
+  const res = await fetch('https://notion-api.splitbee.io/v1/page/180aa21a2f79807b8f08cbcb8370c282', { cache: "no-store" });
   const data = await res.json();
 
   return <>
